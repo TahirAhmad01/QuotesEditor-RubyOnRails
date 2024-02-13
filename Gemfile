addda32f6cb4c126e7694f9ec23fb8b0b371eeae
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-13 18:29:33 +0600

    device and user model created

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ group :test do
   gem "selenium-webdriver"
 end
 gem "simple_form", "~> 5.1.0"
+gem "devise", "~> 4.8.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :quotes
   # root "quotes#index", as: "quotes_index"
   get "up" => "rails/health#show", as: :rails_health_check
