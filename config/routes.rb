4e60038b028b0ddbb80189663d60e1a70dc3a580
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-19 17:55:26 +0600

    Nested Turbo Frames

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root to: "pages#index"
   devise_for :users
   resources :quotes
+
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
