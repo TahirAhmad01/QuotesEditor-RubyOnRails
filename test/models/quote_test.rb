8841f80682f835dece55732c39a61a8a3acd9e1b
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-20 10:52:52 +0600

    Adding a quote total with Turbo Frames

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,3 +1,7 @@
-class Quote < ApplicationRecord
-  validates :name, presence: true
+require "test_helper"
+
+class QuoteTest < ActiveSupport::TestCase
+  test "#total_price returns the sum of the total price of all line items" do
+    assert_equal 2500, quotes(:first).total_price
+  end
 end
