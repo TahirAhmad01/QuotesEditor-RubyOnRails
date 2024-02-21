5c4f09f4807fb6d50faab1fd8af648bd3974577a
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-21 13:05:32 +0600

    master key

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,9 +2,6 @@
 # exit on error
 set -o errexit
 
-rm -rf config/credentials.yml.enc
-EDITOR="mate --wait" bin/rails credentials:edit
-
 yarn install --force
 bundle install
 bundle exec rails assets:precompile
