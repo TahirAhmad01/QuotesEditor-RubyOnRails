8ac82253f00ba1a1434794d94122637e45c97f8d
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-23 10:59:23 +0600

    db change sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_02_19_105358) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "companies", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
